a0ab0af87e36b57cd3bd6a672329ad4cb591ca6d
ES4A <info@es4a.de>
2021-09-01 01:01:43 +0200

    Create effectEngine.cpp

---
+++ src/effectEngine.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include "effectEngine.h"
+
+effectEngine::effectEngine(){
+}
+
+effectEngine::effectEngine(int leds){
+}
+
+void effectEngine::setData(String data){
+}
+
+void effectEngine::tick(){
+}
