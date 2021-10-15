989b032c4ac46192fa7ddec5a5a910f45d2ba82a
ES4A <info@es4a.de>
2021-10-15 16:29:36 +0200

    Update effectEngine.cpp

--- src/effectEngine.cpp
+++ src/effectEngine.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "EffectEngine.h"
+#include <Arduino_JSON.h>
 
 EffectEngine::effectEngine(){
 }
@@ -8,7 +9,17 @@ EffectEngine::effectEngine(int leds){
 }
 
 void EffectEngine::setData(String data){
+    effectData = data;
+    //TODO check if effect
+}
+
+void EffectEngine::setBrightness(int brightness){
+    this.brightness = brightness;
 }
 
 void EffectEngine::tick(){
+}
+
+void EffectEngine::setListener(callbackFunction function){
+    updateFunction = function;
 }
