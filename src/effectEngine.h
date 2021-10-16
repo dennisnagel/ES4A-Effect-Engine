2ba39dd9275f557822b76b9bb4728a7551177eb1
ES4A <info@es4a.de>
2021-10-16 02:31:13 +0200

    Update effectEngine.h

--- src/effectEngine.h
+++ src/effectEngine.h
@@ -1,16 +1,16 @@
 #ifndef EffectEngine_h
 	#define EffectEngine_h
 
+	extern "C" {
+		typedef void (*callbackFunction)();
+	}
+
 	#if defined(ARDUINO) && ARDUINO >= 100
 		#include "Arduino.h"
 		#else
 		#include "WProgram.h"
 	#endif 
 	#include <Arduino_JSON.h>
-
-	extern "C" {
-		typedef void (*callbackFunction)();
-	}
 	
 	class EffectEngine{
 		public:	
