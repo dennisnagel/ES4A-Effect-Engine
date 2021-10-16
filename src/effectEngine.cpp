5c29013fd870d5d1264d5d3c55004a29e3b8f786
ES4A <info@es4a.de>
2021-10-16 04:21:12 +0200

    Update effectEngine.cpp

--- src/effectEngine.cpp
+++ src/effectEngine.cpp
@@ -72,60 +72,6 @@ void EffectEngine::HsvToRgb(JSONVar data, uint8_t &r, uint8_t &g, uint8_t &b){
     b = blue;
 }
 
-void EffectEngine::buildPattern(JSONVar data, uint8_t &reddata[], uint8_t &greendata[], uint8_t &bluedata[]){
-    for (size_t i = 0; i < data.length(); i++){
-        if(String((const char*)data[i]["ty"]) == "oc"){
-            uint8_t red, green, blue;
-            HsvToRgb(data[i]["co"], red, green, blue);
-            int start = map((int)data[i]["st"], 0, 1000, 0, ledCount);
-            int count = map((int)data[i]["en"], 0, 1000, 0, ledCount) - start;
-            for (size_t e = 0; e <= count; e++){
-                reddata[e + start][0] = red;
-                greendata[e + start][1] = green;
-                bluedata[e + start][2] = blue;
-            }
-        }
-    }
-}
-
-JSONVar EffectEngine::mergePattern(JSONVar data1, JSONVar data2){
-    for (size_t i = 0; i < ledCount; i++){
-        if((int)data2[i][0] != NULL){
-            data1[i][0] = (int)data2[i][0];
-            data1[i][1] = (int)data2[i][1];
-            data1[i][2] = (int)data2[i][2];
-        }
-    }
-
-    return data1;
-}
-
-void EffectEngine::buildLayer(JSONVar data, int index, uint8_t &reddata[], uint8_t &greendata[], uint8_t &bluedata[]){
-    long procTime = millis() - startTime[index];
-    long checkTime = 0;
-
-    for (size_t i = 0; i < data.length(); i++){
-        if(checkTime < procTime){
-            if(String((const char*)data[i]["ty"]) == "st"){
-                uint8_t reddatap[ledCount] = {};
-                uint8_t greendatap[ledCount] = {};
-                uint8_t bluedatap[ledCount] = {};
-                buildPattern(data[i]["da"], reddatap, greendatap, bluedatap);
-
-                reddata = reddatap;
-                greendata = greendatap;
-                bluedata = bluedatap;
-                //Serial.println(JSON.stringify(pattern));
-                //returnData = mergePattern(returnData, pattern);
-            }
-        }
-        checkTime += (long)data[i]["du"];
-        checkTime += (long)data[i]["de"];
-        if(i == data.length() - 1 && procTime > checkTime){
-            startTime[index] = millis();
-        }
-    }
-}
 
 void EffectEngine::tick(){
     if(effect){
@@ -173,7 +119,6 @@ void EffectEngine::tick(){
                     startTime[index] = millis();
                 }
             }
-            //returnData = mergePattern(returnData, layer);
             //build LayverEnd
 
         }
