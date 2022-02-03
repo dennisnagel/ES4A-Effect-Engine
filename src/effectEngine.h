c7432d55da03edfb6642802e01396e4f3b2ccdef
ES4A <info@es4a.de>
2022-02-03 23:32:46 +0100

    Update effectEngine.h

--- src/effectEngine.h
+++ src/effectEngine.h
@@ -2,7 +2,7 @@
 	#define EffectEngine_h
 
 	extern "C" {
-		typedef void (*callbackFunctionRGB)(uint8_t r[], uint8_t g[], uint8_t b[]);
+		typedef void (*callbackFunctionRGB)(int pos, uint8_t r[], uint8_t g[], uint8_t b[], uint8_t ww[], uint8_t nw[], uint8_t cw[], uint8_t a[]); //rgbwwa
 	}
 
 	#if defined(ARDUINO) && ARDUINO >= 100
@@ -17,11 +17,13 @@
 
 			EffectEngine();
 			EffectEngine(int leds);
+			EffectEngine(int leds, int width);
 		
             void tick();
             void setData(String data);
 			void setBrightness(int brightness);
 			void setListenerRGB(callbackFunctionRGB function);
+			void setColorMode(bool r, bool g, bool b, bool ww, bool nw, bool cw, bool a);
 	
 		private:
 			void HsvToRgb(JSONVar data, uint8_t &r, uint8_t &g, uint8_t &b);
@@ -29,8 +31,16 @@
 
 			JSONVar effectData;
 			callbackFunctionRGB updateFunctionRGB;
+			bool _rActvie = true;
+			bool _gActvie = true;
+			bool _bActvie = true;
+			bool _wwActvie = false;
+			bool _nwActvie = false;
+			bool _cwActvie = false;
+			bool _aActvie = false;
 			int effectBrightness = 100;
 			int ledCount = 60;
+			int _width = 1;
             bool effect = false;
 			long startTime[] = {};
 		
