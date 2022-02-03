de90f1a3033d5dc414efb2c8acb7aeccfd209d7c
ES4A <info@es4a.de>
2022-02-03 23:54:32 +0100

    Update effectEngine.h

--- src/effectEngine.h
+++ src/effectEngine.h
@@ -27,6 +27,7 @@
 	
 		private:
 			void HsvToRgb(JSONVar data, uint8_t &r, uint8_t &g, uint8_t &b);
+			void ColorToRgbwwwa(JSONVar data, uint8_t &r, uint8_t &g, uint8_t &b, uint8_t &ww, uint8_t &nw, uint8_t &cw, uint8_t &a);
 			void buildPattern(JSONVar data, uint8_t reddata[], uint8_t greendata[], uint8_t bluedata[]);
 
 			JSONVar effectData;
